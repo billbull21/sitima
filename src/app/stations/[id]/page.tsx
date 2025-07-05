dca02306d32298a49d41e57d90f640a600600884
khabib <oemampedia@gmail.com>
2025-07-06 01:22:36 +0700

    update setting vscode

--- src/app/stations/[id]/page.tsx
+++ src/app/stations/[id]/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import React, { use } from 'react';
+import { use } from "react";
 
 interface PageProps {
-    params: Promise<{ id: string }>;
+  params: Promise<{ id: string }>;
 }
 
 const Page = ({ params }: PageProps) => {
-    const { id } = use(params);
-    return (
-        <main>
-            <h1>Router Page</h1>
-            <p>Current ID: {id}</p>
-        </main>
-    );
+  const { id } = use(params);
+  return (
+    <main>
+      <h1>Router Page</h1>
+      <p>Current ID: {id}</p>
+    </main>
+  );
 };
 
 export default Page;
